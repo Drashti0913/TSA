276346f2015473b2b0496e76bed51a6e1efba843
Veerangi18 <117500484+Veerangi18@users.noreply.github.com>
2023-04-27 02:10:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,9 @@ def get_sentiment_counts(sentiments):
     for sentiment in sentiments:
        if isinstance(sentiment, tuple):
            sentiment = sentiment[0]
-    if sentiment > 0:
+    if sentiment > '0':
         pos_count += 1
-    elif sentiment < 0:
+    elif sentiment < '0':
         neg_count += 1
     else:
         neu_count += 1
