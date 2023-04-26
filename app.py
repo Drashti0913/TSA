0c76cdf2d060f5e72424575020bf9850ffae7bb6
Veerangi18 <117500484+Veerangi18@users.noreply.github.com>
2023-04-27 02:06:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def get_sentiment_counts(sentiments):
     for sentiment in sentiments:
         if isinstance(sentiment, tuple):
             sentiment = sentiment[0]
-        if sentiment > 0:
+    if sentiment > 0:
             pos_count += 1
         elif sentiment < 0:
             neg_count += 1
@@ -70,18 +70,6 @@ def get_sentiment_counts(sentiments):
 
     return pos_count, neg_count, neu_count
 
-def sentiment_counts(sentiments):
-    pos_count = 0
-    neg_count = 0
-    neu_count = 0
-    for sentiment in sentiments:
-        if sentiment > 0:
-            pos_count += 1
-        elif sentiment < 0:
-            neg_count += 1
-        else:
-            neu_count += 1
-    return pos_count, neg_count, neu_count
 
 def main():
 	st.title("Sentiment Analysis NLP App")
@@ -145,7 +133,7 @@ def main():
 				st.write(tweets_df)
 				# Analyze sentiment
 				sentiments = analyze_sentiment(tweets_df['clean_text'])
-				pos_count, neg_count, neu_count = sentiment_counts(sentiments)
+				pos_count, neg_count, neu_count = get_sentiment_counts(sentiments)
 				st.write("Sentiment Analysis")
 				st.write("Positive: ", pos_count)
 				st.write("Negative: ", neg_count)
