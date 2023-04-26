6598f9d1f449871bc2b2aceae5dc046ba77ae8a1
Veerangi18 <117500484+Veerangi18@users.noreply.github.com>
2023-04-27 02:40:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,95 +52,94 @@ def analyze_sentiment(text):
             sentiments.append(('neutral', sentiment))
     return sentiments
 
-
 def get_sentiment_counts(sentiments):
-    pos_count = 0
-    neg_count = 0
-    neu_count = 0
-    for sentiment in sentiments:
-        if sentiment > 0:
-            pos_count += 1
-        elif sentiment < 0:
-            neg_count += 1
-        else:
-            neu_count += 1
-    return pos_count, neg_count, neu_count
+    pos_count = sum(1 for sentiment in sentiments if sentiment[0] == 'positive')
+    neg_count = sum(1 for sentiment in sentiments if sentiment[0] == 'negative')
+    neu_count = sum(1 for sentiment in sentiments if sentiment[0] == 'neutral')
+    tweet_sentiments = [(tweet, sentiment) for tweet, sentiment in sentiments]
+    return pos_count, neg_count, neu_count, tweet_sentiments
 
 
 def main():
-	st.title("Sentiment Analysis NLP App")
-	st.subheader("Streamlit Projects")
+    st.title("Sentiment Analysis NLP App")
+    st.subheader("Streamlit Projects")
 
-	menu = ["Home", "Extract from Twitter", "About"]
-	choice = st.sidebar.selectbox("Menu", menu)
+    menu = ["Home", "Extract from Twitter", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
 
-	if choice == "Home":
-		st.subheader("Home")
-		with st.form(key='nlpForm'):
-			raw_text = st.text_area("Enter Text Here")
-			submit_button = st.form_submit_button(label='Analyze')
+    if choice == "Home":
+        st.subheader("Home")
+        with st.form(key='nlpForm'):
+            raw_text = st.text_area("Enter Text Here")
+            submit_button = st.form_submit_button(label='Analyze')
 
-		# layout
-		col1,col2 = st.columns(2)
-		if submit_button:
+        # layout
+        col1,col2 = st.columns(2)
+        if submit_button:
 
-			with col1:
-				st.info("Results")
-				sentiment = TextBlob(raw_text).sentiment
-				st.write(sentiment)
+            with col1:
+                st.info("Results")
+                sentiment = TextBlob(raw_text).sentiment
+                st.write(sentiment)
 
-				# Emoji
-				if sentiment.polarity > 0:
-					st.markdown("Sentiment:: Positive :smiley: ")
-				elif sentiment.polarity < 0:
-					st.markdown("Sentiment:: Negative :angry: ")
-				else:
-					st.markdown("Sentiment:: Neutral 😐 ")
+                # Emoji
+                if sentiment.polarity > 0:
+                    st.markdown("Sentiment:: Positive :smiley: ")
+                elif sentiment.polarity < 0:
+                    st.markdown("Sentiment:: Negative :angry: ")
+                else:
+                    st.markdown("Sentiment:: Neutral 😐 ")
 
-				# Dataframe
-				result_df = convert_to_df(sentiment)
-				st.dataframe(result_df)
+                # Dataframe
+                result_df = convert_to_df(sentiment)
+                st.dataframe(result_df)
 
-				# Visualization
-				c = alt.Chart(result_df).mark_bar().encode(
-					x='metric',
-					y='value',
-					color='metric')
-				st.altair_chart(c,use_container_width=True)
+                # Visualization
+                c = alt.Chart(result_df).mark_bar().encode(
+                    x='metric',
+                    y='value',
+                    color='metric')
+                st.altair_chart(c,use_container_width=True)
 
-			with col2:
-				st.info("Token Sentiment")
-				token_sentiments = analyze_token_sentiment(raw_text)
-				st.write(token_sentiments)
+            with col2:
+                st.info("Token Sentiment")
+                token_sentiments = analyze_token_sentiment(raw_text)
+                st.write(token_sentiments)
 
-	elif choice == "Extract from Twitter":
-		st.subheader("Extract from Twitter")
-		with st.form(key='twitterForm'):
-			keyword = st.text_input("Enter keyword to search on Twitter")
-			num_tweets = st.number_input("Enter number of tweets to fetch", min_value=1, max_value=1000, step=1)
-			submit_button = st.form_submit_button(label='Extract')
+    elif choice == "Extract from Twitter":
+        st.subheader("Extract from Twitter")
+        with st.form(key='twitterForm'):
+            keyword = st.text_input("Enter keyword to search on Twitter")
+            num_tweets = st.number_input("Enter number of tweets to fetch", min_value=1, max_value=1000, step=1)
+            submit_button = st.form_submit_button(label='Extract')
 
-		if submit_button:
-			# Fetch tweets
-			tweets_df = extract_tweets(keyword, num_tweets)
-			if not tweets_df.empty:
-				st.write(tweets_df)
-				# Analyze sentiment
-				sentiments = analyze_sentiment(tweets_df['clean_text'])
-				pos_count, neg_count, neu_count = get_sentiment_counts(sentiments)
-				st.write("Sentiment Analysis")
-				st.write("Positive: ", pos_count)
-				st.write("Negative: ", neg_count)
-				st.write("Neutral: ", neu_count)
-				# Pie chart
-				pie_data = {'Positive': pos_count, 'Negative': neg_count, 'Neutral': neu_count}
-				pie_df = pd.DataFrame.from_dict(pie_data, orient='index', columns=['count'])
-				fig = px.pie(pie_df, values='count', names=pie_df.index, title='Sentiment Distribution')
-				st.plotly_chart(fig)
-
-				
-			else:
-				st.warning("No tweets found.")
+	if submit_button:
+	    # Fetch tweets
+	    tweets_df = extract_tweets(keyword, num_tweets)
+	    if not tweets_df.empty:
+		st.write(tweets_df)
+		# Analyze sentiment
+		sentiments = analyze_sentiment(tweets_df['clean_text'])
+		pos_count, neg_count, neu_count, tweet_sentiments = get_sentiment_counts(sentiments)
+		st.write("Sentiment Analysis")
+		st.write("Positive: ", pos_count)
+		st.write("Negative: ", neg_count)
+		st.write("Neutral: ", neu_count)
+		st.write("Tweet Sentiments:")
+		for tweet, sentiment, polarity in tweet_sentiments:
+		    if sentiment == 'positive':
+			st.markdown(f"> {tweet} :smiley:")
+		    elif sentiment == 'negative':
+			st.markdown(f"> {tweet} :angry:")
+		    else:
+			st.markdown(f"> {tweet} 😐")
+		# Pie chart
+		pie_data = {'Positive': pos_count, 'Negative': neg_count, 'Neutral': neu_count}
+		pie_df = pd.DataFrame.from_dict(pie_data, orient='index', columns=['count'])
+		fig = px.pie(pie_df, values='count', names=pie_df.index, title='Sentiment Distribution')
+		st.plotly_chart(fig)
+	    else:
+		st.warning("No tweets found.")
 
 	else:
 		st.subheader("About")
