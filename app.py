78d63b3f36ba925dcf696f6bce82b3d03c8be8f6
Veerangi18 <117500484+Veerangi18@users.noreply.github.com>
2023-04-27 01:43:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,13 +51,14 @@ def get_sentiment_counts(sentiments):
     neg_count = 0
     neu_count = 0
     for sentiment in sentiments:
-        if sentiment.polarity > 0:
+        if sentiment > 0:
             pos_count += 1
-        elif sentiment.polarity < 0:
+        elif sentiment < 0:
             neg_count += 1
         else:
             neu_count += 1
     return pos_count, neg_count, neu_count
+
 
 
 def main():
