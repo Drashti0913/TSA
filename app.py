7e62b535736efb4674ffaf6084671d2fc71c113e
Veerangi18 <117500484+Veerangi18@users.noreply.github.com>
2023-04-28 09:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,6 @@ def main():
                 sentiments = analyze_sentiment(tweets_df['clean_text'])
                 sentiments = ["positive", "negative", "neutral", "positive", "positive"]
                 counts = get_sentiment_counts(sentiments)
-                pos_count, neg_count, neu_count = counts
                 st.write("Sentiment Analysis")
                 st.write("Positive: ", pos_count)
                 st.write("Negative: ", neg_count)
